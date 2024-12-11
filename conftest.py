b4ea2e18325e37a39bb313d0ddc709217ca83dc4
nadezhda.demina <N.Demina@transtelematica.ru>
2024-12-11 23:08:56 +0400

    Доработан conftest.py. Внесены комментарии к фикстурам

--- conftest.py
+++ conftest.py
@@ -3,18 +3,22 @@ from selenium import webdriver
 
 users = {
     "user": {"username": "standard_user", "password": "secret_sauce"},
-    "locked_user": {"username": "locked_out_user", "password": "secret_sauce"},
-    "error_user": {"username": "error_user", "password": "secret_sauce"}}
+    "log_out_user": {"username": "locked_out_user", "password": "secret_sauce"},
+    "user_to_view": {"username": "visual_user", "password": "secret_sauce"}}
 
+# пользоватей с обычной авторизацией
 selected_user = "user"
 right_user = users[selected_user]
 
-wrong_user = "error_user"
-forbidden_user = users[wrong_user]
+# заблокированный пользователь
+forbidden_user = "log_out_user"
+unauthorized_user = users[forbidden_user]
 
-log_out_user = "locked_user"
-unauthorized_user = users[log_out_user]
+# пользователь с правом просмотра
+user_for_view = "user_to_view"
+user_to_views = users[user_for_view]
 
+# ссылка приложения
 test_url = "https://www.saucedemo.com/"
 
 
@@ -28,40 +32,43 @@ def browser():
     driver.quit()
 
 
+# фикстура для авторизации пользователя с корректным логином и паролем
 @pytest.fixture
 def authorize_right_user(browser):
     url = test_url
     browser.get(url)
     login = browser.find_element("xpath", '//div[@class="form_group"]//input[@placeholder="Username"]')
     password = browser.find_element("xpath", '//div[@class="form_group"]//input[@placeholder="Password"]')
-    enter = browser.find_element("xpath", '//input[@data-test="login-button"]//parent::*')
+    enter = browser.find_element("xpath", '//div[@class="login-box"]//input[@type="submit"]')
 
     login.send_keys(right_user["username"])
     password.send_keys(right_user["password"])
     enter.click()
 
 
-@pytest.fixture
-def authorize_forbidden_user(browser):
-    url = test_url
-    browser.get(url)
-    login = browser.find_element("xpath", '//div[@class="form_group"]//input[@placeholder="Username"]')
-    password = browser.find_element("xpath", '//div[@class="form_group"]//input[@placeholder="Password"]')
-    enter = browser.find_element("xpath", '//input[@data-test="login-button"]//parent::*')
-
-    login.send_keys(forbidden_user["username"])
-    password.send_keys(forbidden_user["password"])
-    enter.click()
-
-
+# фикстура для авторизации заблокированного пользователя
 @pytest.fixture
 def authorize_log_out_user(browser):
     url = test_url
     browser.get(url)
     login = browser.find_element("xpath", '//div[@class="form_group"]//input[@placeholder="Username"]')
     password = browser.find_element("xpath", '//div[@class="form_group"]//input[@placeholder="Password"]')
-    enter = browser.find_element("xpath", '//input[@data-test="login-button"]//parent::*')
+    enter = browser.find_element("xpath", '//div[@class="login-box"]//input[@type="submit"]')
 
     login.send_keys(unauthorized_user["username"])
     password.send_keys(unauthorized_user["password"])
+    enter.click()
+
+
+# фикстура для авторизации пользователя с правом просмотра
+@pytest.fixture
+def authorize_user_to_views(browser):
+    url = test_url
+    browser.get(url)
+    login = browser.find_element("xpath", '//div[@class="form_group"]//input[@placeholder="Username"]')
+    password = browser.find_element("xpath", '//div[@class="form_group"]//input[@placeholder="Password"]')
+    enter = browser.find_element("xpath", '//div[@class="login-box"]//input[@type="submit"]')
+
+    login.send_keys(user_to_views["username"])
+    password.send_keys(user_to_views["password"])
     enter.click()
